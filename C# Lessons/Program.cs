d98a2cfa23ab744b4833582664ad0de5addf0c7f
Yulia Egorova <youlkin@mail.ru>
2022-02-06 14:19:58 +0300

    homework 2 done

--- C# Lessons/Program.cs
+++ C# Lessons/Program.cs
@@ -7,8 +7,9 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write(n * n);*/
 
 
-//1. По двум заданным числам проверять является ли первое квадратом второго
 
+
+//1. По двум заданным числам проверять является ли первое квадратом второго
 
 /*Console.Write("Введите первое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -21,6 +22,8 @@ Console.Write("Число " + n + " является квадратом числ
 else {
    Console.Write("Число " + n + " не является квадратом числа " + n2); 
 }*/
+
+
 
 
 //2. Даны два числа. Показать большее и меньшее число
@@ -38,6 +41,9 @@ else {
     Console.WriteLine("Число "  + n2  + " большее");
     Console.Write("Число "  + n  + " меньшее");
 }*/
+
+
+
 
 //3. По заданному номеру дня недели вывести его название
 
@@ -61,6 +67,8 @@ if (day == 1) {
 } else {
     Console.WriteLine("Дня недели с таким номером нет.");
 }*/
+
+
 
 
 //4. Найти максимальное из трех чисел
@@ -87,6 +95,9 @@ else {
 }*/
 
 
+
+
+
 //5. Написать программу вычисления значения функции y = f(a)
 
 // y = a^2 + 2
@@ -103,6 +114,9 @@ double a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine($"Значение y {function(a)}");*/
 
 
+
+
+
 //6. Выяснить является ли число чётным
 
 /*Console.Write("Введите число: ");
@@ -114,6 +128,8 @@ if (num % 2 == 0) {
 else {
     Console.WriteLine("Число " + num + " нечетное"); 
 }*/
+
+
 
 
 //7. Показать числа от -N до N
@@ -129,18 +145,23 @@ while (nMin <= nMax) {
 }*/
 
 
+
+
 //8. Показать четные числа от 1 до N
 
 /*Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result = 1;
 
-while (result <= number && result != 0) {   
+while (result <= number) {   
     result ++;
     if(result % 2 == 0){
         Console.WriteLine(result);
     }
 }*/
+
+
+
 
 //9. Показать последнюю цифру трёхзначного числа
 
@@ -148,6 +169,9 @@ while (result <= number && result != 0) {
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Последняя цифра числа " + number + " равна " + number % 10);*/
+
+
+
 
 
 //10. Показать вторую цифру трёхзначного числа
@@ -158,7 +182,141 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Предпоследняя цифра числа " + number + " равна " + number % 100 / 10);*/
 
 
+
+
+
 //11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+/*Console.WriteLine("Введите число от 10 до 99: ");
+int num = int.Parse(Console.ReadLine());
+
+int first = num / 10;
+int second = num % 10;
+
+if (first > second) {
+    Console.Write("Наибольша цифра числа " + num + " = " + first);
+}
+else {
+  Console.Write("Наибольша цифра числа " + num + " = " + second);
+}*/
+
+
+
+
+
 //12. Удалить вторую цифру трёхзначного числа
+
+/*int Number = new Random().Next(100, 999);
+Console.WriteLine("Вторая цифра трехзначного числа: " + Number + " это цифра " + Number %100 / 10);
+int num1 = Number / 100;
+int num2 = Number % 10;
+Console.WriteLine("Удаляем вторую цифру числа " + Number + " получаем число " + num1 + num2);*/
+//Console.WriteLine("Первая цифра числа " + Number + " это " + num1);
+//Console.WriteLine("Последняя цифра числа " + Number + " это " + num2);
+
+
+
+
+
 //13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+/*int Number1 = new Random().Next(1, 100);
+int Number2 = new Random().Next(1, 100);
+Console.WriteLine("Первое заданное число: " + Number1);
+Console.WriteLine("Второе заданное число: " + Number2);
+
+if(Number1 < Number2) {
+    Console.Write("Число " + Number1 + " меньше числа  " +  Number2);
+}
+else if(Number1 % Number2 == 0) {
+    Console.WriteLine("Число " + Number1 + " кратно числу " + Number2);
+}
+else {
+    Console.Write("Заданное число " + Number1 + " не кратно числу " + Number2 + " ,остаток от деления = " +  Number1 % Number2);
+}*/
+
+
+
+
+
 //14. Найти третью цифру числа или сообщить, что её нет
+
+/*int number = new Random().Next(1, 5000);
+
+if (number < 100)
+    Console.WriteLine("Третья цифра в числе " + number + " отсуствует");
+else if(number >= 100 && number <= 999)
+{
+    Console.WriteLine("Третья цифра числа " + number + " это " + number % 10);
+}
+else {
+    Console.WriteLine("Третья цифра числа " +  number + " это " + number % 100 / 10);
+}*/
+
+
+
+
+
+//Почувствуй себя джуном*
+//15. Дано число. Проверить кратно ли оно 7 и 23
+
+//Вариант 1: 
+/*int Number = new Random().Next(1, 100);
+
+if(Number % 7 == 0 && Number % 23 == 0) 
+Console.WriteLine("Число " + Number + " кратно обоим числам: и 7 и 23 ");
+else if(Number % 7 == 0)
+{
+    Console.WriteLine("Число " + Number + " кратно числу 7, но не кратно 23");
+}
+else if(Number % 23 == 0)
+{
+    Console.WriteLine("Число " + Number + " кратно числу 23, но не кратно 7");
+}
+else
+{
+    Console.Write("Число " + Number + " не кратно обоим числам : ни 7, ни 23");
+}*/
+
+//Вариант 2: 
+
+//int Number1 = new Random().Next(1, 5000); //Такой  вариант тоже возможен 
+/*int Number1 = 500;
+for(int i = 1; i < Number1; i++) {
+    if(i % 7 == 0 && i % 23 == 0)
+    Console.WriteLine($"Число {i} кратно числам 7 и 23");
+}*/
+
+
+
+
+//16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+string[] WeekDays = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+/*int[] WeekDaysNum = new int[WeekDays.Length];
+for(int i = 0; i < WeekDays.Length; i++) {
+    WeekDaysNum[i] = int.Parse(WeekDays[i]);
+}
+for(int i = 0; i < WeekDaysNum.Length; i++) {
+    Console.WriteLine(WeekDaysNum[i]);
+}*/
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 | day == 7) {
+  Console.WriteLine("День недели номер " + day + " является выходным");  
+} else if (day == 2 | day == 1 | day == 3 | day == 4 | day == 5) {
+    Console.WriteLine("Этот день - будний");
+} else {
+    Console.WriteLine("Дня недели с таким номером нет.");
+}
+
+
+
+
+/*17. По двум заданным числам проверять является ли одно квадратом другого
+18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+20. Задать номер четверти, показать диапазоны для возможных координат
+21. Программа проверяет пятизначное число на палиндромом.
+22. Найти расстояние между точками в пространстве 2D/3D*/
