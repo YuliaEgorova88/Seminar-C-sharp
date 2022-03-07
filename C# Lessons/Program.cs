073e770559c6cfb7c3a3b8c41f70e2b0f599c39d
Yulia Egorova <youlkin@mail.ru>
2022-03-07 15:53:42 +0300

    done with friend's help

--- C# Lessons/Program.cs
+++ C# Lessons/Program.cs
@@ -2879,3 +2879,27 @@ PrintNum(dataOne, infoOne);
 
 Задача: найти M при заданном N и получить одно из разбиений на группы N ≤ 10²⁰*/
 
+
+Console.Write("Введите N: ");
+int num = Int32.Parse(Console.ReadLine());
+
+
+FuncGroup(num);
+
+
+void FuncGroup(int num)
+{
+    int group = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        if (i == Math.Pow(2, group))
+        {
+            Console.WriteLine();
+            group++;
+            Console.Write($"Группа {group}: ");
+        }
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
